66ab41b3e1ff156a43508e3e07f659953ccae349
Ahmed Al Darabee <seahmednail@gmail.com>
2025-07-28 15:04:12 +0300

    initialization to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen min-h-screen flex justify-content-center align-items-center'>
+      <h3 className="font-bold">hello world!</h3>
     </div>
   );
 }
